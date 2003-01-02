fa21d39600be05606191b3a9a834b7b75a4dec5d
obi <obi>
2003-01-02 02:52:49 +0000

    support for all known bmon versions

--- ppcboot/common/cmd_debugmode.c
+++ ppcboot/common/cmd_debugmode.c
@@ -30,65 +30,72 @@
 
 #if (CONFIG_COMMANDS & CFG_CMD_DEBUGMODE)
 
-static unsigned long *product_ptr(void)
+struct product {
+	char mid;
+	char ver[3];
+	int *addr;
+} __attribute__ ((packed));
+
+static struct product models[7] = {
+	{ 1, "1.0", (int *) 0x10000944 },
+	{ 1, "1.2", (int *) 0x1000095c },
+	{ 2, "1.0", (int *) 0x1000091c },
+	{ 3, "1.0", (int *) 0x10000a00 },
+	{ 3, "1.1", (int *) 0x10000904 },
+	{ 3, "1.2", (int *) 0x10000904 },
+	{ 3, "1.3", (int *) 0x100008d8 },
+};
+
+static char *manufacturers[3] = {
+	"nokia",
+	"philips",
+	"sagem"
+};
+
+static int bmon_version(unsigned char *ver) {
+
+	char *bmon = (char *) 0x10000000;
+	char *p;
+
+	for (p = bmon; p < bmon + 131072; p++)
+		if ((p - bmon + 11 <= 131072) && (!memcmp(p, "dbox2:", 6))) {
+			memcpy(ver, &p[8], 3);
+			return 0;
+		}
+
+	return -1;
+}
+
+
+static int *product_ptr(void)
 {
-	switch (((char*)0x1001ffe0)[0]) {
-	case 1:
-		if (!strncmp((char*)0x100146c8, "1.0", 3)) {
-			printf("nokia bmon 1.0\n");
-			return (unsigned long*)0x10000944;
-		}
-		if (!strncmp((char*)0x10014a88, "1.2", 3)) {
-			printf("nokia bmon 1.2\n");
-			return (unsigned long*)0x1000095c;
-		}
-		break;
+	char mid = ((char *) 0x1001ffe0)[0];
+	char ver[3];
+	int i;
 
-	case 2:
-		if (!strncmp(((char*)0x100152a4), "1.0", 3)) {
-			printf("philips bmon 1.0\n");
-			return (unsigned long*)0x1000091c;
-		}
-		break;
-
-	case 3:
-#if 0
-		if (!strncmp((char*)0x10015418, "1.0", 3)) { /* FIXME: wrong address */
-			printf("sagem bmon 1.0\n");
-			return (unsigned long*)0x10000a00;
-		}
-#endif
-		if (!strncmp((char*)0x10015418, "1.1", 3)) {
-			printf("sagem bmon 1.1\n");
-			return (unsigned long*)0x10000904;
-		}
-		if (!strncmp((char*)0x10015418, "1.2", 3)) {
-			printf("sagem bmon 1.2\n");
-			return (unsigned long*)0x10000904;
-		}
-#if 0
-		if (!strncmp((char*)0x10015418, "1.3", 3)) { /* FIXME: wrong address */
-			printf("sagem bmon 1.0\n");
-			return (unsigned long*)0x100008d8;
-		}
-#endif
-		break;
-
-	default:
-		break;
+	if (bmon_version(ver) < 0) {
+		printf("unable to detect bmon version\n");
+		return NULL;
 	}
-	
-	printf("unknown bootloader, please report!\n");
+
+	for (i = 0; i < 7; i++) {
+		if ((models[i].mid == mid) && (!memcmp(models[i].ver, ver, 3))) {
+			printf("%s bmon %c%c%c\n", manufacturers[mid - 1], ver[0], ver[1], ver[2]);
+			return models[i].addr;
+		}
+	}
+
+	printf("unknown %s bmon version %c%c%c. please report!\n", manufacturers[mid - 1], ver[0], ver[1], ver[2]);
 	return NULL;
 }
 
-static void debugmode_status(unsigned long *product)
+static void debugmode_status(int *product)
 {
-	switch(product[0]) {
-	case 0x00000000:
+	switch (product[0]) {
+	case 0:
 		printf("debug mode is enabled\n");
 		break;
-	case 0xffffffff:
+	case -1:
 		printf("debug mode is disabled\n");
 		break;
 	default:
@@ -97,31 +104,30 @@ static void debugmode_status(unsigned long *product)
 	}
 }
 
-static void debugmode_enable(unsigned long *product)
+static void debugmode_enable(int *product)
 {
 	printf("+++ debugmode_enable\n");
-	//product[0] = 0x00000000;
+	//product[0] = 0;
 }
 
-static void debugmode_disable(unsigned long *product)
+static void debugmode_disable(int *product)
 {
 	printf("+++ debugmode_disable\n");
-	//product[0] = 0xffffffff;
+	//product[0] = -1;
 }
 
 static int on_off(const char *s)
 {
-	if (strcmp(s, "on") == 0) {
-		return (1);
-	} else if (strcmp(s, "off") == 0) {
-		return (0);
-	}
-	return (-1);
+	if (!strcmp(s, "on"))
+		return 1;
+	else if (!strcmp(s, "off"))
+		return 0;
+	return -1;
 }
 
 void do_debugmode(cmd_tbl_t *cmdtp, bd_t *bd, int flag, int argc, char *argv[])
 {
-	unsigned long *product = product_ptr();
+	int *product = product_ptr();
 
 	if (!product) {
 		printf("could not identify product\n");
