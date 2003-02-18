b962157419b41ef67061263ba23ab02d4561cf1f
waldi <waldi>
2003-02-18 17:03:50 +0000

    remove from HEAD, not longer needed

--- ppcboot/dbox/lcd-font.h
+++
@@ -1,3 +0,0 @@
-#define LCD_PIXEL_KEEP	3
-
-void lcd_print(int x, int y, char *text, unsigned char c0, unsigned char c1);
--- ppcboot/tools/gdb/error.h
+++
@@ -1,7 +0,0 @@
-#include <stdarg.h>
-
-extern char *pname;
-
-extern void Warning(char *, ...);
-extern void Error(char *, ...);
-extern void Perror(char *, ...);
